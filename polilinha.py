119b9e27d3e022b4a5b008292f5dc47471105d28
Deydson Costa <103591633+falloutdey@users.noreply.github.com>
2025-09-10 21:28:12 -0300

    correção dos bugs no recortePoligono e recorteLinha

--- polilinha.py
+++ polilinha.py
@@ -9,20 +9,18 @@ class Polilinha:
     def __init__(self, pontos: list, fechar=False):
         self.saida = []
         
-        # Cria uma cópia da lista de pontos para não modificar a original
         pontos_a_desenhar = list(pontos)
 
-        # Se a opção 'fechar' for verdadeira, adiciona o primeiro ponto ao final
-        # da lista para criar a linha de fechamento.
         if fechar and len(pontos_a_desenhar) > 1:
             pontos_a_desenhar.append(pontos_a_desenhar[0])
 
-        # Se houver pelo menos dois pontos, desenha as linhas
         if len(pontos_a_desenhar) > 1:
             for i in range(len(pontos_a_desenhar) - 1):
-                ponto_inicial = pontos_a_desenhar[i]
-                ponto_final = pontos_a_desenhar[i+1]
+                # Arredonda as coordenadas para inteiros antes de desenhar
+                ponto_inicial = [round(pontos_a_desenhar[i][0]), round(pontos_a_desenhar[i][1])]
+                ponto_final = [round(pontos_a_desenhar[i+1][0]), round(pontos_a_desenhar[i+1][1])]
                 
-                # Usa o algoritmo de Bresenham para cada segmento
-                linha = Bresenham(ponto_inicial, ponto_final)
-                self.saida.extend(linha.saida)
+                # Desenha o segmento apenas se os pontos forem diferentes após o arredondamento
+                if ponto_inicial != ponto_final:
+                    linha = Bresenham(ponto_inicial, ponto_final)
+                    self.saida.extend(linha.saida)
--- recorteLinha.py
+++ recorteLinha.py
@@ -1,29 +1,37 @@
-# recorte_linha.py
+# recorteLinha.py
 from bresenham import Bresenham
 
 class RecorteLinha:
     def __init__(self, ponto_1, ponto_2, xmin, ymin, xmax, ymax):
         self.saida = []
-        self.xmin = xmin
-        self.xmax = xmax
-        self.ymin = ymin
-        self.ymax = ymax
+        self.xmin = float(xmin)
+        self.xmax = float(xmax)
+        self.ymin = float(ymin)
+        self.ymax = float(ymax)
 
         self.ACIMA = 8
         self.ABAIXO = 4
         self.DIREITA = 2
         self.ESQUERDA = 1
         
-        self.cohen_sutherland(list(ponto_1), list(ponto_2))
+        # Converte os pontos de entrada para float para manter a precisão
+        p1_float = [float(ponto_1[0]), float(ponto_1[1])]
+        p2_float = [float(ponto_2[0]), float(ponto_2[1])]
+        
+        self.cohen_sutherland(p1_float, p2_float)
 
     def _calcular_codigo(self, ponto):
         x, y = ponto
         codigo = 0
         if x < self.xmin: codigo |= self.ESQUERDA
-        elif x > self.xmax: codigo |= self.DIREITA
+        elif x > self.xmax - 1: codigo |= self.DIREITA
         if y < self.ymin: codigo |= self.ABAIXO
-        elif y > self.ymax: codigo |= self.ACIMA
+        elif y > self.ymax - 1: codigo |= self.ACIMA
         return codigo
+
+    def _limitar_valor(self, valor, min_val, max_val):
+        """Garante que o valor fique dentro do intervalo [min_val, max_val-1]."""
+        return max(min_val, min(valor, max_val - 1))
 
     def cohen_sutherland(self, ponto_1, ponto_2):
         codigo1 = self._calcular_codigo(ponto_1)
@@ -31,43 +39,42 @@ class RecorteLinha:
         aceite = False
 
         while True:
-            if codigo1 == 0 and codigo2 == 0:
+            if codigo1 == 0 and codigo2 == 0:  # Ambos dentro
                 aceite = True
                 break
-            elif (codigo1 & codigo2) != 0:
+            elif (codigo1 & codigo2) != 0:  # Ambos fora e na mesma região
                 break
-            else:
+            else:  # Recorte necessário
                 x, y = 0.0, 0.0
                 codigo_fora = codigo1 if codigo1 != 0 else codigo2
                 x1, y1 = ponto_1
                 x2, y2 = ponto_2
                 
                 if codigo_fora & self.ACIMA:
-                    x = x1 + (x2 - x1) * (self.ymax - y1) / (y2 - y1) if y1 != y2 else x1
-                    y = float(self.ymax)
+                    x = x1 + (x2 - x1) * (self.ymax - 1 - y1) / (y2 - y1) if y1 != y2 else x1
+                    y = self.ymax - 1
                 elif codigo_fora & self.ABAIXO:
                     x = x1 + (x2 - x1) * (self.ymin - y1) / (y2 - y1) if y1 != y2 else x1
-                    y = float(self.ymin)
+                    y = self.ymin
                 elif codigo_fora & self.DIREITA:
-                    y = y1 + (y2 - y1) * (self.xmax - x1) / (x2 - x1) if x1 != x2 else y1
-                    x = float(self.xmax)
+                    y = y1 + (y2 - y1) * (self.xmax - 1 - x1) / (x2 - x1) if x1 != x2 else y1
+                    x = self.xmax - 1
                 elif codigo_fora & self.ESQUERDA:
                     y = y1 + (y2 - y1) * (self.xmin - x1) / (x2 - x1) if x1 != x2 else y1
-                    x = float(self.xmin)
+                    x = self.xmin
                 
-                nx, ny = round(x), round(y)
-                
-                nx = max(self.xmin, min(self.xmax, nx))
-                ny = max(self.ymin, min(self.ymax, ny))
-
                 if codigo_fora == codigo1:
-                    ponto_1[0], ponto_1[1] = nx, ny
+                    ponto_1[0], ponto_1[1] = x, y
                     codigo1 = self._calcular_codigo(ponto_1)
                 else:
-                    # --- CORREÇÃO DO BUG AQUI ---
-                    ponto_2[0], ponto_2[1] = nx, ny # Estava 'ny, ny'
+                    ponto_2[0], ponto_2[1] = x, y
                     codigo2 = self._calcular_codigo(ponto_2)
 
         if aceite:
-            linha_recortada = Bresenham(ponto_1, ponto_2)
+            # Arredonda para inteiros apenas no final e limita dentro da janela
+            p1_int = [self._limitar_valor(round(ponto_1[0]), self.xmin, self.xmax),
+                      self._limitar_valor(round(ponto_1[1]), self.ymin, self.ymax)]
+            p2_int = [self._limitar_valor(round(ponto_2[0]), self.xmin, self.xmax),
+                      self._limitar_valor(round(ponto_2[1]), self.ymin, self.ymax)]
+            linha_recortada = Bresenham(p1_int, p2_int)
             self.saida = linha_recortada.saida
